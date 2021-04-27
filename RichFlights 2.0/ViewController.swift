0c51e982cbc37f8bc031108b2d37eab054e1c9d5
Tendaishe Gwini <tgmsstoney@gmail.com>
2021-04-27 09:55:13 -0400

    Initial Commit

---
+++ RichFlights 2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RichFlights 2.0
+//
+//  Created by Tendaishe Gwini on 4/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
