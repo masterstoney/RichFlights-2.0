b437e749a6ff7db3475059ff3b1f01537427c8dd
Tendaishe Gwini <tgmsstoney@gmail.com>
2021-06-04 13:52:18 -0400

    Added screenshots

---
+++ RichFlights 2.0/Helper/UIStackViewExtension.swift
@@ -0,0 +1,23 @@
+//
+//  UIStackViewExtension.swift
+//  RichFlights 2.0
+//
+//  Created by Tendaishe Gwini on 6/2/21.
+//
+
+import UIKit
+
+extension UIStackView {
+    
+    func removeFully(view: UIView) {
+        removeArrangedSubview(view)
+        view.removeFromSuperview()
+    }
+    
+    func removeFullyAllArrangedSubviews() {
+        arrangedSubviews.forEach { (view) in
+            removeFully(view: view)
+        }
+    }
+    
+}
